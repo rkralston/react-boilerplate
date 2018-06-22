318b8ba843384b5b71ec9c1b9a2e548053665226
Keith Ralston <rkralston@gmail.com>
2018-06-22 10:29:24 -0500

    Routing interim commit

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -9,7 +9,7 @@ import React from 'react';
 import Link from '../Link';
 import styles from './styles.css';
 
-function LinkList({ links }) {
+function LinkList({ links, routeTopicName }) {
   const linkNodes = links.map(l => (
     <Link
       key={l.id}
@@ -19,12 +19,14 @@ function LinkList({ links }) {
 
   return (
     <div className={styles.linkList}>
+      {routeTopicName}
       {linkNodes}
     </div>
   );
 }
 
 LinkList.propTypes = {
+  routeTopicName: React.PropTypes.string.isRequired,
   links: React.PropTypes.arrayOf(React.PropTypes.shape({
     description: React.PropTypes.string.isRequired,
     url: React.PropTypes.string.isRequired,
--- app/containers/LinkListContainer/index.js
+++ app/containers/LinkListContainer/index.js
@@ -8,8 +8,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import selectLinkListContainer from './selectors';
 import LinkList from '../../components/LinkList';
+import requestLinks from './actions';
 
 export class LinkListContainer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    routeTopicName: React.PropTypes.string.isRequired,
+    requestLinks: React.PropTypes.func.isRequired,
+  }
+  componentWillMount() {
+    this.props.requestLinks(this.props.routeTopicName);
+  }
   render() {
     return (
       <div>
@@ -23,7 +31,7 @@ const mapStateToProps = selectLinkListContainer();
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    requestLinks: (topicName) => dispatch(requestLinks(topicName)),
   };
 }
 
